79b9ccb009d250da6076a2faf56ba2e7474f3c02
HuangYuanHuang <834773759@qq.com>
2016-12-28 16:57:19 +0800

    Revert "Revert "WordReport""
    This reverts commit 99b596dd576a5214b055156e24f72d3159bc8369.

---
+++ WordReportCore/TableModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordReportCore
+{
+    public class TableModel
+    {
+        /// <summary>
+        /// word模板 tablestart 名称
+        /// </summary>
+        public string ListName { get; set; }
+
+        /// <summary>
+        /// 列集合
+        /// </summary>
+        public string[] ColumsName { get; set; }
+
+        public List<List<TableColumnNode>> Rows { get; set; }
+
+        public TableModel()
+        {
+            Rows = new List<List<TableColumnNode>>();
+        }
+    }
+
+    public class TableColumnNode
+    {
+        public string ColumnName { get; set; }
+
+        public object Value { get; set; }
+    }
+}
